afc185b15a4c9f3a666c6b4731093a0dd0af197e
mqhelem <155308250+mqhelem@users.noreply.github.com>
2024-05-19 13:04:08 +0200

    Weather.js and css updates

--- src/App.js
+++ src/App.js
@@ -1,48 +1,13 @@
 import "./App.css";
+import Weather from "./Weather";
 
 import Forecast from "./Forecast";
 
 function App() {
   return (
-    <div class="weather-app">
-      <form id="searchForm">
-        <input class="city" id="city" placeholder="Enter your city" />
-        <input class="submit" type="submit" value="Search" />
-      </form>
-
-      <div class="weather">
-        <div>
-          <h1 class="current-city" id="current-city">
-            Paris
-          </h1>
-          <p class="weather-details">
-            <span id="time"></span>, <span id="description"></span> <br />
-            Humidity: <strong id="humidity"></strong>, Wind:
-            <strong id="wind"></strong>
-          </p>
-        </div>
-        <div class="current-temperature">
-          <img
-            class="icon"
-            id="icon"
-            src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
-          />
-          <span class="temperature" id="temperature"></span>
-          <span class="unit">°C</span>
-        </div>
-      </div>
-      <div class="weather-forecast" id="forecast"></div>
-      <Forecast />
-      <footer>
-        Coded by
-        <a href="https://github.com/mqhelem">Nomqhele Mhlanga,</a> source code
-        hosted in
-        <a href="https://github.com/mqhelem/weather-react">Github,</a>
-        app available at
-        <a href="https://weatherfourcast.netlify.app/">Netlify</a>
-      </footer>
+    <div className="App">
+      <Weather />
     </div>
   );
 }
-
 export default App;
--- src/Weather.js
+++ src/Weather.js
@@ -1,2 +1,66 @@
+import React, { useState } from "react";
+import axios from "axios";
 
+export default function Weather(props) {
+  let [city, setCity] = useState("Johannesburg");
+  let [loaded, setLoaded] = useState(false);
+  let [weather, setWeather] = useState({});
 
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  function showWeatherDetails(response) {
+    setLoaded(true);
+    setWeather({
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+    });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "8d9838178b5b401f1b4e7cb5af18e210";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(showWeatherDetails);
+  }
+  let form = (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter your city"
+          className="cityName"
+          onChange={updateCity}
+        />
+        <input type="submit" value="Search city" />
+      </form>
+      <hr></hr>
+      <h1>{city}</h1>
+    </div>
+  );
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <h1>{setCity}</h1>
+        <div className="weather">
+          <p className="details">
+            Temperature: <strong>{Math.round(weather.temperature)}℃ </strong>,
+            Humidity: <strong>{Math.round(weather.humidity)}%</strong>
+            <br />
+            Description: {weather.description}, Wind:{" "}
+            <strong>
+              {Math.round(weather.wind)}
+              km/hr
+            </strong>
+          </p>
+          <img class="icon" src={weather.icon} alt={weather.description} />
+        </div>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
